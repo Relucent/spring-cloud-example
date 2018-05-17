1bb6085884608d04bdf1f4cb78f6637cbd1e6dbf
Galanodel <relucent@163.com>
2018-05-17 15:51:36 +0800

    #

--- spring-cloud-example-provider-upload-8004/src/main/java/yyl/springcloud/controller/FileUploadController.java
+++ spring-cloud-example-provider-upload-8004/src/main/java/yyl/springcloud/controller/FileUploadController.java
@@ -1,5 +1,8 @@
 package yyl.springcloud.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,12 +15,17 @@ public class FileUploadController {
 	// ~ http://localhost:8004/upload
 	@PostMapping("/upload")
 	@ResponseBody
-	public String upload(@RequestParam(value = "file", required = true) MultipartFile file) {
+	public Object upload(@RequestParam(value = "file", required = true) MultipartFile file) {
 		String originalFilename = file.getOriginalFilename();
+		String contentType = file.getContentType();
+		long size = file.getSize();
 		System.out.println("OriginalFilename" + file.getOriginalFilename());
-		// byte[] bytes = file.getBytes();
-		// FileCopyUtils.copy(in, out);
-		return originalFilename;
+		// FileCopyUtils.copy(file.getInputStream(), out);
+		Map<String,Object> result = new HashMap<>();
+		result.put("originalFilename", originalFilename);
+		result.put("contentType", contentType);
+		result.put("size", size);
+		return result;
 	}
 
 }
